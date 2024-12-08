18d2116c5f00e433958c7ad46d789bb1e2484641
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-08 20:04:50 +0000

    Fetch global quote in API service and set the Stock object from DTO in stock service

--- src/main/java/com/sahil/stock/info/service/ApiService.java
+++ src/main/java/com/sahil/stock/info/service/ApiService.java
@@ -12,13 +12,14 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class ApiService {
-    @Value("${stock.api_key}")
-    private String apiKey;    
-    private WebClient webClient = WebClient.builder().baseUrl(apiKey).build();
+    @Value("${stock.api_url}")
+    private String apiUrl;
+    
+    private WebClient webClient = WebClient.builder().baseUrl(apiUrl).build();
 
-    public Mono<GlobalQuoteDto> getGlobalQuote(String endpoint) {
+    public Mono<GlobalQuoteDto> getGlobalQuote(String symbol) {
         return webClient.get()
-                        .uri(endpoint)
+                        .uri("function=GLOBAL_QUOTE&datatype=json&symbol=" + symbol)
                         .retrieve()
                         .bodyToMono(GlobalQuoteDto.class)
                         .doOnError(e -> log.error("Error occurred: " + e.getMessage()))
--- src/main/java/com/sahil/stock/info/service/StockService.java
+++ src/main/java/com/sahil/stock/info/service/StockService.java
@@ -1,54 +1,44 @@
 package com.sahil.stock.info.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sahil.stock.info.model.Stock;
-import com.sahil.stock.info.repository.StockRepository;
+import com.sahil.stock.info.dto.GlobalQuoteDto;
+
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Service
 @Transactional
 @Slf4j
 public class StockService {
     @Autowired
-    private StockRepository stockRepository;
+    private ApiService apiService;
 
-    public Stock createStock(String symbol) {
-        if (stockRepository.findBySymbol(symbol).isEmpty()) {
+    public Stock getGlobalQuote(String symbol) {
+        Mono<GlobalQuoteDto> dtoMono = apiService.getGlobalQuote(symbol);
+        if (dtoMono != null) {
+            log.info("DTO fetched: " + dtoMono.toString());
             Stock stock = new Stock();
-            // Set other details.
-            stock.setSymbol(symbol);
-            stockRepository.save(stock);
-            log.info("Stock created: " + stock.toString());
-            return stock; 
+            dtoMono.map(dto -> {
+                stock.setSymbol(dto.getSymbol());
+                stock.setOpen(dto.getOpen());
+                stock.setHigh(dto.getHigh());
+                stock.setLow((dto.getLow()));
+                stock.setPrice((dto.getPrice()));
+                stock.setVolume((dto.getVolume()));
+                stock.setPrevClose((dto.getPrevClose()));
+                stock.setChange((dto.getChange()));
+                stock.setChangePercent((dto.getChangePercent()));
+                stock.setFetchedAt(new Date());
+                return stock;
+            });
         }
-        Stock stock = stockRepository.findBySymbol(symbol).get();
-        log.info("Stock already exists: " + stock.toString());
-        // Update it.
         return null;
-    }
-
-    public Stock getStock(String symbol) {
-        if (stockRepository.findBySymbol(symbol).isPresent()) {
-            Stock stock = stockRepository.findBySymbol(symbol).get();
-            log.info("Stock found: " + stock.toString());
-            return stock;
-        }
-        log.info("Stock not found: " + symbol);
-        return null;
-    }
-
-    public boolean deleteStock(String symbol) {
-        if (stockRepository.findBySymbol(symbol).isPresent()) {
-            Stock stock = stockRepository.findBySymbol(symbol).get();
-            stockRepository.delete(stock);
-            log.info("Stock deleted: " + symbol);
-            return true;
-        }
-        log.info("Stock not found: " + symbol);
-        return false;
     }
 }
