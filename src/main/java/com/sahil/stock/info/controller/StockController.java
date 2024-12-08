e93d3b822ad0f2916dbdb2523d1f25994c37be08
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-08 20:06:39 +0000

    Update stock controller

--- src/main/java/com/sahil/stock/info/controller/StockController.java
+++ src/main/java/com/sahil/stock/info/controller/StockController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,37 +27,19 @@ public class StockController {
         return String.format(
                 "Stock Info Service%n%n" +
                         "Welcome to the stock endpoint, you can make the following requests:%n" +
-                        "- POST /new-stock%n" +
-                        "- GET /get-stock%n" +
-                        "- DELETE /delete-stock%n");
+                        "- GET /get-global-quote%n" +
+                        "- GET /get-intraday%n" +
+                        "- GET /get-daily%n" +
+                        "- GET /get-weekly%n" +
+                        "- GET /get-monthly%n");
     }
 
-    @PostMapping(value = "/new-stock", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Stock> createStock(@RequestBody String name) {
-        log.info("Received request to POST /new-stock with argument: " + name.trim());
-        Stock stock = stockService.createStock(name.trim());
-        if (stock != null) {
-            return ResponseEntity.ok(stock);
-        }
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
-    }
-
-    @GetMapping(value = "/get-stock", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Stock> getStock(@RequestBody String name) {
-        log.info("Received request to GET /get-stock with argument: " + name.trim());
-        Stock stock = stockService.getStock(name.trim());
+    @GetMapping(value = "/get-global-quote", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Stock> getGlobalQuote(@RequestBody String symbol) {
+        log.info("Received request to GET /get-global-quote with argument: " + symbol.trim());
+        Stock stock = stockService.getGlobalQuote(symbol.trim());
         if (stock != null) {
             return ResponseEntity.ok(stock);            
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
-
-    @DeleteMapping(value = "/delete-stock", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> deleteStock(@RequestBody String name) {
-        log.info("Received request to DELETE /delete-stock with argument: " + name.trim());
-        boolean response = stockService.deleteStock(name.trim());
-        if (response) {
-            return ResponseEntity.ok("Stock deleted successfully.");
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
