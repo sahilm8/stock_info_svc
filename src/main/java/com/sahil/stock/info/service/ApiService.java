d4efac9153a6414e30617a7294a33bc118d963aa
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-08 19:17:45 +0000

    Remove API config, setup getGlobalQuote in API service

--- src/main/java/com/sahil/stock/info/service/ApiService.java
+++ src/main/java/com/sahil/stock/info/service/ApiService.java
@@ -1,5 +1,6 @@
 package com.sahil.stock.info.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,13 +12,11 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class ApiService {
-    private final WebClient webClient;
-    
-    public ApiService(WebClient webClient) {
-        this.webClient = webClient;
-    }
+    @Value("${stock.api_key}")
+    private String apiKey;    
+    private WebClient webClient = WebClient.builder().baseUrl(apiKey).build();
 
-    public Mono<GlobalQuoteDto> fetchGlobalQuote(String endpoint) {
+    public Mono<GlobalQuoteDto> getGlobalQuote(String endpoint) {
         return webClient.get()
                         .uri(endpoint)
                         .retrieve()
