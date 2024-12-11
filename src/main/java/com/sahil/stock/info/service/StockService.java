52cca955cffe837754606c32d03dc7a421b69491
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-11 03:40:34 +0000

    Move api service logic to stock service

--- src/main/java/com/sahil/stock/info/service/StockService.java
+++ src/main/java/com/sahil/stock/info/service/StockService.java
@@ -2,8 +2,9 @@ package com.sahil.stock.info.service;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sahil.stock.info.model.Stock;
 import com.sahil.stock.info.dto.GlobalQuoteDto;
@@ -15,28 +16,29 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class StockService {
-    @Autowired
-    private ApiService apiService;
+    @Value("${stock.api_url}")
+    private String apiUrl;
+    
+    private WebClient webClient = WebClient.builder().baseUrl("https://www.alphavantage.co/query").build();
 
-    public Stock getGlobalQuote(String symbol) {
-        Mono<GlobalQuoteDto> dtoMono = apiService.getGlobalQuote(symbol);
-        if (dtoMono != null) {
-            log.info("DTO fetched: " + dtoMono.toString());
-            Stock stock = new Stock();
-            dtoMono.map(dto -> {
+    public Mono<Stock> getGlobalQuote(String symbol) {
+        return webClient.get()
+            .uri("?apikey=Q4JWW7S4SWVHSWDS&function=GLOBAL_QUOTE&datatype=json&symbol=" + symbol)
+            .retrieve()
+            .bodyToMono(GlobalQuoteDto.class)
+            .map(dto -> {
+                Stock stock = new Stock();
                 stock.setSymbol(dto.getSymbol());
                 stock.setOpen(dto.getOpen());
                 stock.setHigh(dto.getHigh());
-                stock.setLow((dto.getLow()));
-                stock.setPrice((dto.getPrice()));
-                stock.setVolume((dto.getVolume()));
-                stock.setPrevClose((dto.getPrevClose()));
-                stock.setChange((dto.getChange()));
-                stock.setChangePercent((dto.getChangePercent()));
+                stock.setLow(dto.getLow());
+                stock.setPrice(dto.getPrice());
+                stock.setVolume(dto.getVolume());
+                stock.setPrevClose(dto.getPrevClose());
+                stock.setChange(dto.getChange());
+                stock.setChangePercent(dto.getChangePercent());
                 stock.setFetchedAt(new Date());
                 return stock;
             });
-        }
-        return null;
     }
 }
