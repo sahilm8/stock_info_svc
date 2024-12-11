ecec7fc68c02c362a6ca1c79403973adfb6baf23
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-11 22:08:30 +0000

    Add getTimeSeriesIntraday & getIntervalDtoClass, format getGlobalQuote

--- src/main/java/com/sahil/stock/info/service/StockService.java
+++ src/main/java/com/sahil/stock/info/service/StockService.java
@@ -1,16 +1,27 @@
 package com.sahil.stock.info.service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sahil.stock.info.model.Stock;
+import com.sahil.stock.info.model.StockTs;
+import com.sahil.stock.info.model.TimeSeries;
 import com.sahil.stock.info.util.ApiFunctions;
 
 import jakarta.annotation.PostConstruct;
 
 import com.sahil.stock.info.dto.GlobalQuoteDto;
-
+import com.sahil.stock.info.dto.TsIntraday15MinDto;
+import com.sahil.stock.info.dto.TsIntraday1MinDto;
+import com.sahil.stock.info.dto.TsIntraday30MinDto;
+import com.sahil.stock.info.dto.TsIntraday5MinDto;
+import com.sahil.stock.info.dto.TsIntraday60MinDto;
+import com.sahil.stock.info.dto.TsIntradayDto;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -34,10 +45,10 @@ public class StockService {
     public Mono<Stock> getGlobalQuote(String symbol) {
         return webClient.get()
             .uri(uriBuilder -> uriBuilder
-            .queryParam("apikey", apiKey)
             .queryParam("function", ApiFunctions.GLOBAL_QUOTE.getValue())
-            .queryParam("datatype", "json")
             .queryParam("symbol", symbol)
+            .queryParam("datatype", "json")
+            .queryParam("apikey", apiKey)
             .build())  
             .retrieve()
             .bodyToMono(GlobalQuoteDto.class)
@@ -56,4 +67,52 @@ public class StockService {
                 return stock;
             });
     }
+
+    public Mono<TimeSeries> getTimeSeriesIntraday(String symbol, String interval) {
+        return webClient.get()
+            .uri(uriBuilder -> uriBuilder
+            .queryParam("function", ApiFunctions.TIME_SERIES_INTRADAY.getValue())
+            .queryParam("symbol", symbol)
+            .queryParam("interval", interval)
+            .queryParam("adjusted", false)
+            .queryParam("extended_hours", true)
+            .queryParam("outputsize", "compact")
+            .queryParam("datatype", "json")
+            .queryParam("apikey", apiKey)
+            .build())
+            .retrieve()
+            .bodyToMono(getIntervalDtoClass(interval))
+            .map(dto -> {
+                TimeSeries timeSeries = new TimeSeries();
+                Map<String, StockTs> ts = new HashMap<>();
+                for (Map.Entry<String, Map<String, String>> entry : dto.getTimeSeries().entrySet()) {
+                    StockTs stockTs = new StockTs();
+                    stockTs.setOpen(new BigDecimal(entry.getValue().get(dto.getOpen(entry.getKey()))));
+                    stockTs.setHigh(new BigDecimal(entry.getValue().get(dto.getHigh(entry.getKey()))));
+                    stockTs.setLow(new BigDecimal(entry.getValue().get(dto.getLow(entry.getKey()))));
+                    stockTs.setClose(new BigDecimal(entry.getValue().get(dto.getClose(entry.getKey()))));
+                    stockTs.setVolume(new BigDecimal(entry.getValue().get(dto.getVolume(entry.getKey()))));
+                    ts.put(entry.getKey(), stockTs);
+                }
+                timeSeries.setTimeSeries(ts);
+                return timeSeries;
+            });
+    }
+
+    private Class<? extends TsIntradayDto> getIntervalDtoClass(String interval) {
+        switch (interval) {
+            case "1min":
+            return TsIntraday1MinDto.class;
+            case "5min":
+            return TsIntraday5MinDto.class;
+            case "15min":
+            return TsIntraday15MinDto.class;
+            case "30min":
+            return TsIntraday30MinDto.class;
+            case "60min":
+            return TsIntraday60MinDto.class;
+            default:
+            return TsIntradayDto.class;
+        }
+    } 
 }
