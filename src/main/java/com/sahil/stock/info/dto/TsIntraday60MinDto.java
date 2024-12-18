cfe60ce8f4c5a4943759a4aee8ccae7c38e8f4a0
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2024-12-18 18:07:01 +0000

    Extend TsIntradayDto abstract class for different intervals

--- src/main/java/com/sahil/stock/info/dto/TsIntraday60MinDto.java
+++ src/main/java/com/sahil/stock/info/dto/TsIntraday60MinDto.java
@@ -5,12 +5,35 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
 @EqualsAndHashCode(callSuper=false)
 public class TsIntraday60MinDto extends TsIntradayDto {
     @JsonProperty("Time Series (60min)")
     private Map<String, Map<String, String>> timeSeries = new HashMap<>();
+
+    @Override
+    public String getOpen(String timestamp) {
+            return timeSeries.get(timestamp).get("1. open");
+    }
+
+    @Override
+    public String getHigh(String timestamp) {
+        return timeSeries.get(timestamp).get("2. high");
+    }
+
+    @Override
+    public String getLow(String timestamp) {
+        return timeSeries.get(timestamp).get("3. low");
+    }
+
+    @Override
+    public String getClose(String timestamp) {
+        return timeSeries.get(timestamp).get("4. close");
+    }
+
+    @Override
+    public String getVolume(String timestamp) {
+        return timeSeries.get(timestamp).get("5. volume");
+    }
 }
