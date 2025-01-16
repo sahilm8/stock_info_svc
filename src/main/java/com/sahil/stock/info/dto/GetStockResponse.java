d4972494c28e9319948dc4b05034baa664162cc1
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-16 12:14:12 +0000

    Create DTOs for getStock request & response

--- src/main/java/com/sahil/stock/info/dto/GetStockResponse.java
+++ src/main/java/com/sahil/stock/info/dto/GetStockResponse.java
@@ -1,34 +1,21 @@
 package com.sahil.stock.info.dto;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class GlobalQuoteDto {
-    /*
-    API Response:
-    {
-        "Global Quote": {
-        "01. symbol": "IBM",
-        "02. open": "234.4300",
-        "03. high": "238.3800",
-        "04. low": "234.2200",
-        "05. price": "238.0400",
-        "06. volume": "4028430",
-        "07. latest trading day": "2024-12-06",
-        "08. previous close": "234.7500",
-        "09. change": "3.2900",
-        "10. change percent": "1.4015%"
-        }
-    }
-    */
-
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetStockResponse {
     @JsonProperty("Global Quote")
-    private Map<String, String> globalQuote = new HashMap<>();
+    private Map<String, String> globalQuote;
 
     public String getSymbol() {
         return globalQuote.get("01. symbol");
