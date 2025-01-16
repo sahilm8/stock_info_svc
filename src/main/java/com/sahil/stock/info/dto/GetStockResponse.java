67cda61904356c8a898dd47ddd8a3fed2d826619
Sahil Memon <177644884+sahilm8@users.noreply.github.com>
2025-01-16 15:05:49 +0000

    Update GetStockResponse DTO

--- src/main/java/com/sahil/stock/info/dto/GetStockResponse.java
+++ src/main/java/com/sahil/stock/info/dto/GetStockResponse.java
@@ -14,46 +14,74 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetStockResponse {
+    /*
+     * API Response:
+     * {
+     * "Global Quote": {
+     * "01. symbol": "IBM",
+     * "02. open": "234.4300",
+     * "03. high": "238.3800",
+     * "04. low": "234.2200",
+     * "05. price": "238.0400",
+     * "06. volume": "4028430",
+     * "07. latest trading day": "2024-12-06",
+     * "08. previous close": "234.7500",
+     * "09. change": "3.2900",
+     * "10. change percent": "1.4015%"
+     * }
+     * }
+     */
+
     @JsonProperty("Global Quote")
     private Map<String, String> globalQuote;
 
+    @JsonProperty("symbol")
     public String getSymbol() {
         return globalQuote.get("01. symbol");
     }
 
+    @JsonProperty("open")
     public String getOpen() {
-        return new String(globalQuote.get("02. open"));
+        return globalQuote.get("02. open");
     }
 
+    @JsonProperty("high")
     public String getHigh() {
-        return new String(globalQuote.get("03. high"));
+        return globalQuote.get("03. high");
     }
 
+    @JsonProperty("low")
     public String getLow() {
-        return new String(globalQuote.get("04. low"));
+        return globalQuote.get("04. low");
     }
 
+    @JsonProperty("price")
     public String getPrice() {
-        return new String(globalQuote.get("05. price"));
+        return globalQuote.get("05. price");
     }
 
+    @JsonProperty("volume")
     public String getVolume() {
-        return new String(globalQuote.get("06. volume"));
+        return globalQuote.get("06. volume");
     }
 
+    @JsonProperty("latestTradingDay")
     public String getLatestTradingDay() {
         return globalQuote.get("07. latest trading day");
     }
 
+    @JsonProperty("previousClose")
     public String getPrevClose() {
-        return new String(globalQuote.get("08. previous close"));
+        return globalQuote.get("08. previous close");
     }
 
+    @JsonProperty("change")
     public String getChange() {
-        return new String(globalQuote.get("09. change"));
+        return globalQuote.get("09. change");
     }
 
+    @JsonProperty("changePercent")
     public String getChangePercent() {
-        return new String(globalQuote.get("10. change percent"));
+        return globalQuote.get("10. change percent");
     }
 }
